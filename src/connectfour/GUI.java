00ba6bd9d3d6aaeab87a6964cdf95152b4e2dd34
Tyler Hou <thou@exeter.edu>
2015-02-20 11:35:08 -0500

    added undo functions aand started ai class

--- src/connectfour/GUI.java
+++ src/connectfour/GUI.java
@@ -12,8 +12,13 @@ public class GUI {
 		while (b.getWinner() == null) {
 			System.out.println("  0   1   2   3   4   5   6"); // type in
 			System.out.println(b);
-			if ((c=cin.nextInt()) <= 6 && c >= 0)
+			if ((c=cin.nextInt()) <= 6 && c >= -1)
+				if (c == -1) {
+					b.undo();
+				}
+				else {
 				b.move(c);
+				}
 		}
 		System.out.println(b.getWinnerColor());
 		System.out.println(b.getWinner());
