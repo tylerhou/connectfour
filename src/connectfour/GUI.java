0aec6199060407c39d058a18cceba3eaf77c46d5
Alan Liu <AL8991@gmail.com>
2015-02-20 11:05:33 -0500

    test commit

--- src/connectfour/GUI.java
+++ src/connectfour/GUI.java
@@ -9,7 +9,7 @@ public class GUI {
 		Scanner cin = new Scanner(System.in);
 		Board b = new Board();
 		while (b.getWinner() == null) {
-			System.out.println("  0   1   2   3   4   5   6");
+			System.out.println("  0   1   2   3   4   5   6"); // type in
 			System.out.println(b);
 			b.move(cin.nextInt());
 		}
