5bd57433ea8b0a90d03facf9d8b7d9495999ebaa
Alan Liu <AL8991@gmail.com>
2015-02-21 00:49:29 -0500

    added more

--- src/connectfour/TheRealGUI.java
+++ src/connectfour/TheRealGUI.java
@@ -5,45 +5,53 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
 
 public class TheRealGUI extends JFrame implements ActionListener{
 
 	GUI cow = new GUI();
 	private JButton[] board=new JButton[7];
+	private JLabel[][] labels=new JLabel[7][6];
 	private JLabel label;
 	private JFrame frame;
 	private JTable table;
 	
 	public TheRealGUI(){
 	
-
 	setLayout(null);
 	
-	/*frame=new JFrame();
-	add(frame);*/
-	String[] names={"1","2","3"};
-	
-	
+	/*Object[][] data={{"1"},{"2"},{"3"}};
+	String[] columnNames={"1","2","3"};
+	JTable table = new JTable(data, columnNames);
+	table.setBackground(Color.green);
+	could've used JTable
+	*/
+
+	for(int a=0;a<7;a++)
+	{
+		for(int b=0;b<6;b++)
+		{
+			labels[a][b]= new JLabel("a");
+			add(labels[a][b]);	
+			labels[a][b].setBounds(40+a*150,b*100,100,40);
+		}
+	}
 	
 	label=new JLabel("test");
 	label.setBounds(500,200,200,400);
 	add(label);
-	
+
 	for(int a=0;a<board.length;a++)
 	{
 			board[a]= new JButton (" ");
 			add(board[a]);
-			board[a].setBounds(40+a*150,500,100,50);
+			board[a].setBounds(40+a*150,575,100,40);
 			board[a].setActionCommand("" + a + "");
 			board[a].addActionListener(this);
 	}
 	
 
 	
-	setSize(1100,600);  
+	setSize(1100,700);  
 	setVisible(true); 
 	setTitle("Connect Four");
 	setResizable(false);
